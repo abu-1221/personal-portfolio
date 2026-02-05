af54052a7e21c30133cbec01be32598e06e30585
Abu Backer Siddique M <siddique2k6@gmail.com>
2026-02-05 12:08:02 +0530

    Add profile photo and update config for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/personal-portfolio',
+  output: "export",
   images: {
     unoptimized: true,
   },
